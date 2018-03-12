f8a532fe190c982374ce1072940c676477fec5e3
Leonid Tsarev <leo@bastilia.ru>
2018-03-12 12:41:45 +0300

    add testcase

---
+++ test-any-async-modified-closure/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Data.Entity;
+using System.Threading.Tasks;
+
+namespace test_any_async_modified_closure
+{
+    class Entity
+    {
+        public Guid Id { get; set; } 
+    }
+    class DbContext
+    {
+        public DbSet<Entity> Entities { get; set; }
+    }
+
+    static class Program
+    {
+        private static DbContext _ctx;
+
+        public static async Task Main()
+        {
+            _ctx = new DbContext();
+            Guid id = new Guid();
+            await _ctx.Entities.AnyAsync(x => x.Id == id);
+            id = new Guid();
+        }
+    }
+}
